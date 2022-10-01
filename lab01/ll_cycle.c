2214158ec0ca1452554f5be60529b24f49b4c6a1
TMAstrider <t.m.a.strider@gmail.com>
2022-10-01 16:56:27 +0800

    implemented lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) return 0;
+
+    node *hare = head;
+    node *totorise = head;
+    // while (hare->next->next != NULL  && hare->next != NULL){
+    while (hare->next != NULL  && hare->next->next != NULL){
+        /* code */
+        hare = hare->next->next;
+        totorise = totorise->next;
+        if (hare == totorise) {
+            return 1;
+        }
+    }
     return 0;
 }
