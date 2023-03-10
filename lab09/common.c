aa004456354d7000ecf395e3353b0c4a21afcba4
TMAstrider <t.m.a.strider@gmail.com>
2023-03-10 20:38:42 +0800

    done with lab09

--- lab09/common.c
+++ lab09/common.c
@@ -52,9 +52,29 @@ long long int sum_simd(unsigned int vals[NUM_ELEMS]) {
 	/* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
 	
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-		/* YOUR CODE GOES HERE */
 
+		/* YOUR CODE GOES HERE */
+		__m128i sum = _mm_setzero_si128();
+		int output[4];
+
+		for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+			__m128i loadedData = _mm_loadu_si128(( __m128i*)&vals[i]);
+			__m128i flagOver127 = _mm_cmpgt_epi32(loadedData, _127);
+			__m128i subUnder127 = _mm_and_si128(loadedData, flagOver127);
+			sum = _mm_add_epi32(subUnder127, sum);
+		}
+		
 		/* You'll need a tail case. */
+
+		_mm_storeu_si128((__m128i *)output, sum);
+		for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
+		for (unsigned int i = 0; i < 4; i++) {
+      		result += output[i];
+    	}
 
 	}
 	clock_t end = clock();
@@ -69,6 +89,43 @@ long long int sum_simd_unrolled(unsigned int vals[NUM_ELEMS]) {
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
 		/* COPY AND PASTE YOUR sum_simd() HERE */
 		/* MODIFY IT BY UNROLLING IT */
+
+		__m128i sum = _mm_setzero_si128();
+		int output[4];
+
+		for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+			__m128i loadedData = _mm_loadu_si128(( __m128i*)&vals[i]);
+			__m128i flagOver127 = _mm_cmpgt_epi32(loadedData, _127);
+			__m128i subUnder127 = _mm_and_si128(loadedData, flagOver127);
+			sum = _mm_add_epi32(subUnder127, sum);
+
+			loadedData = _mm_loadu_si128(( __m128i*)&vals[i + 4]);
+			flagOver127 = _mm_cmpgt_epi32(loadedData, _127);
+			subUnder127 = _mm_and_si128(loadedData, flagOver127);
+			sum = _mm_add_epi32(subUnder127, sum);
+
+			loadedData = _mm_loadu_si128(( __m128i*)&vals[i + 8]);
+			flagOver127 = _mm_cmpgt_epi32(loadedData, _127);
+			subUnder127 = _mm_and_si128(loadedData, flagOver127);
+			sum = _mm_add_epi32(subUnder127, sum);
+
+			loadedData = _mm_loadu_si128(( __m128i*)&vals[i + 12]);
+			flagOver127 = _mm_cmpgt_epi32(loadedData, _127);
+			subUnder127 = _mm_and_si128(loadedData, flagOver127);
+			sum = _mm_add_epi32(subUnder127, sum);
+		}
+		
+		/* You'll need a tail case. */
+
+		_mm_storeu_si128((__m128i *)output, sum);
+		for(unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
+		for (unsigned int i = 0; i < 4; i++) {
+      		result += output[i];
+    	}
 
 		/* You'll need 1 or maybe 2 tail cases here. */
 
